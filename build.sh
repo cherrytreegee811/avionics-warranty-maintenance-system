0a8ec2caf8a46e513e0d2dafec87e3945cff4825
CherryTreeGee811 <jtaylor5162@conestogac.on.ca>
2026-03-27 15:03:44 -0400

    Update build.sh to fix code styling

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Build script for Avionics Warranty Maintenance System
 
+python -m venv venv
+source ./venv/bin/activate
+pip install -r requirements.txt
+
+cmake-format --config-files .cmake-format -i $(find . -name "CMakeLists.txt" -type f)
+
+find . -name "*.cpp" -o -name "*.h" | grep -v "./build" | grep -v "./_deps" | grep -v "./cpm_modules" | xargs clang-form
+
 set -e
 
 if [ ! -d "build" ]; then
