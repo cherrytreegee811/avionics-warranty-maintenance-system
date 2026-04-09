b5abb94489318369a07bae41147a1528577262d5
CherryTreeGee811 <jtaylor5162@conestogac.on.ca>
2026-04-09 17:46:00 -0400

    Apply Linting

--- source/aircraft/Aircraft.cpp
+++ source/aircraft/Aircraft.cpp
@@ -7,6 +7,7 @@
 #include <common/Packet.h>
 #include <common/WarrantyData.h>
 #include <spdlog/spdlog.h>
+
 #include <algorithm>
 #include <asio.hpp>
 #include <chrono>
--- test/source/mma/MMAIntegrationTests.cpp
+++ test/source/mma/MMAIntegrationTests.cpp
@@ -5,6 +5,7 @@
 #include <mma/mma.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/spdlog.h>
+
 #include <chrono>
 #include <thread>
 
